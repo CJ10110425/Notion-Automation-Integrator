e5a6488b91516e7f58cd000b7d0be5d1bc0ac135
李品澤 <ze0966747312@gmail.com>
2023-11-22 21:33:53 +0800

    Complete all function

--- community_crawlers/taichung/split_community_associations.py.py
+++ community_crawlers/taichung/split_community_associations.py.py
@@ -4,21 +4,33 @@ import pandas as pd
 file_path = '/Users/lipinze/Desktop/Notion-Automation-Integrator/community_associations.csv'
 community_associations_df = pd.read_csv(file_path)
 
+# Function to extract district from name
+
+
+def extract_district(name):
+    # List of all districts
+    all_districts = [
+        "中區", "東區", "西區", "南區", "北區", "西屯區", "南屯區", "北屯區", "豐原區", "大里區",
+        "太平區", "東勢區", "大甲區", "清水區", "沙鹿區", "梧棲區", "后里區", "神岡區", "潭子區",
+        "大雅區", "新社區", "石岡區", "外埔區", "大安區", "烏日區", "大肚區", "龍井區", "霧峰區", "和平區"
+    ]
+    for district in all_districts:
+        if district in name:
+            return district
+    return None
+
+
 # Extracting the district information from the 'name' column
-community_associations_df['district'] = community_associations_df['name'].apply(lambda x: x.split('區')[0][-2:] + '區')
+community_associations_df['district'] = community_associations_df['name'].apply(
+    extract_district)
 
-# List of districts to filter and save as separate CSV files
-districts = [
-    "中區", "東區", "西區", "南區", "北區", "西屯區", "南屯區", "北屯區", "豐原區", "大里區", "太平區", "東勢區",
-    "大甲區", "清水區", "沙鹿區", "梧棲區", "后里區", "神岡區", "潭子區", "大雅區", "新社區", "石岡區", "外埔區",
-    "大安區", "烏日區", "大肚區", "龍井區", "霧峰區", "和平區"
-]
+# Filter and save CSV files for each district
+for district in set(community_associations_df['district']):
+    if district is not None:
+        # Filter the dataframe for the current district
+        district_df = community_associations_df[community_associations_df['district'] == district]
 
-for district in districts:
-    # Filter the dataframe for the current district
-    district_df = community_associations_df[community_associations_df['district'] == district]
-
-    # Save to a new CSV file if there are entries for the district
-    if not district_df.empty:
-        district_file_path = f'/Users/lipinze/Desktop/Notion-Automation-Integrator/{district}.csv'
-        district_df.to_csv(district_file_path, index=False)
+        # Save to a new CSV file if there are entries for the district
+        if not district_df.empty:
+            district_file_path = f'/Users/lipinze/Desktop/Notion-Automation-Integrator/{district}.csv'
+            district_df.to_csv(district_file_path, index=False)
--- notion_functions.py
+++ notion_functions.py
@@ -1,7 +1,8 @@
 import os
 import dotenv
 import requests
-
+import pandas as pd
+import time
 
 def read_notion_database(notion_token, database_id):
     """
@@ -104,7 +105,10 @@ def add_info_to_notion_database(notion_token, database_id, data):
             "對接窗口": {"rich_text": [{"text": {"content": data['對接窗口']}}]},
             "電話": {"phone_number": data['電話']},
             "Email": {"email": data['Email']},
-            "部落名稱": {"title": [{"text": {"content": data['部落名稱'],"link": {"url": data['部落連結']}}}]},
+            "社區名稱": {"title": [{"text": {"content": data['社區名稱']}}]},
+            "職稱": {"rich_text": [{"text": {"content": data['職稱']}}]},
+            "人口數量": {"number": data['人口數量']},
+            "地址": {"rich_text": [{"text": {"content": data['地址']}}]},
             "聯絡進度": {"select": {"name": data['聯絡進度']}},
             "意願程度": {"select": {"name": data['意願程度']}},
             "聯絡方式": {"select": {"name": data['聯絡方式']}}
@@ -113,6 +117,7 @@ def add_info_to_notion_database(notion_token, database_id, data):
 
     response = requests.post(create_page_url, headers=headers, json=page_data)
     return response.json()
+
 
 # Write a function to add many info into database
 def add_many_info_to_notion_database(notion_token, database_id, data_list):
@@ -199,8 +204,39 @@ def create_notion_database(notion_token, parent_page_id, database_title):
     return database_id
 
 def main():
+    districts_with_ids = [
+        {"name": "中區", "id": "db21b6d1-7cec-4ed7-be92-9bc3377e9ed9"},
+        {"name": "東區", "id": "9b8329c7-e496-4ecb-998c-2a313bb3dbb1"},
+        {"name": "西區", "id": "cb277c71-a6fa-41e4-b6f5-b71cffc012ec"},
+        {"name": "南區", "id": "03cda376-5020-4178-bd2a-ea035c0f98d4"},
+        {"name": "北區", "id": "9dddfd64-b79f-4c43-b72b-d6d3afab01db"},
+        {"name": "西屯區", "id": "c1b715ee-a604-4a7b-bc93-771dbcc97bae"},
+        {"name": "南屯區", "id": "30056130-67cb-4bd8-a38d-23759d5362a9"},
+        {"name": "北屯區", "id": "1934473a-fd4e-4720-a1d6-f4bf4d0b2681"},
+        {"name": "豐原區", "id": "9da5cc8c-7f7c-446b-b78f-8e30fc052aa0"},
+        {"name": "大里區", "id": "456c2bc8-1b9b-4c0d-8df4-765a9f7b182d"},
+        {"name": "太平區", "id": "e37416ba-ab10-4a7d-a29b-e0374145a2f3"},
+        {"name": "東勢區", "id": "8aebfab7-fa7a-4d8d-a424-a62169b4f250"},
+        {"name": "大甲區", "id": "6e719c7d-5b68-4be2-aa0a-d8d6ca79759f"},
+        {"name": "清水區", "id": "5611a974-0b85-454d-b4ba-34917cdc55f3"},
+        {"name": "沙鹿區", "id": "cb60bb78-11e5-4e3d-b551-4b1b7f3ebed9"},
+        {"name": "梧棲區", "id": "f5d3b75c-7577-471f-86ef-167f126d0903"},
+        {"name": "后里區", "id": "e3f75277-a6f2-4286-9f38-b6e6841c098a"},
+        {"name": "神岡區", "id": "60751b1d-5fd1-470b-91fe-36315a81b980"},
+        {"name": "潭子區", "id": "0f8be5d3-3efb-492a-9b49-71819841af27"},
+        {"name": "大雅區", "id": "1f7f2da8-bd8b-49e0-a41a-30868f10a063"},
+        {"name": "新社區", "id": "8831e250-038c-4208-abe6-017cca78af69"},
+        {"name": "石岡區", "id": "8197d413-e21a-444f-897a-92f82dfb6209"},
+        {"name": "外埔區", "id": "4c652c26-14a4-40ce-b21a-b669429db49d"},
+        {"name": "大安區", "id": "30f111be-103d-4e40-b19e-23b4e590a741"},
+        {"name": "烏日區", "id": "7a99a34b-318e-47c0-93b5-484534ebd87e"},
+        {"name": "大肚區", "id": "13cef29c-5ab8-4aa7-96ff-7b015b0e5f19"},
+        {"name": "龍井區", "id": "61e6d1df-a748-4538-be74-812dc06e0b47"},
+        {"name": "霧峰區", "id": "ebf2de69-eae0-4ca1-bd6c-fbe2670071ad"},
+        {"name": "和平區", "id": "609f25de-9207-469b-849e-2075f19054e8"}
+    ]
     NOTION_TOKEN = os.getenv("INTERNAL_INTEGRATION_SECRET")
-    DATABASE_ID = os.getenv("DATABASE_ID")
+    # DATABASE_ID = os.getenv("DATABASE_ID")
     PAGE_ID = os.getenv("PAGE_ID")
     # results_json = read_notion_database(NOTION_TOKEN, DATABASE_ID)
     # Process your data using the functions
@@ -214,23 +250,93 @@ def main():
     #     pass
 
     # Test the add_row_to_notion_database function
-    data = {
-        '對接窗口': '張三',
-        '電話': '0912345678',
-        'Email': 'kejcwrkjfh@gmail.com',
-        '部落名稱': '山村部落',
-        '部落連結': 'https://community.society.taichung.gov.tw/compoint/Details.aspx?Parser=99%2C6%2C22%2C%2C%2C%2C16582',
-        '聯絡進度': "待聯絡",
-        '意願程度': 'None',
-        '聯絡方式': 'None'
-    }
-    print(add_info_to_notion_database(NOTION_TOKEN, DATABASE_ID, data))
+    # data = {
+    #         '對接窗口': "ef",
+    #         '電話': "0966747312",
+    #         'Email': ";wlnkef@gmail.com",
+    #         '社區名稱': "wefwf",
+    #         '職稱': 'erge3w',
+    #         '人口數量': 21431,
+    #         '地址': 'fwefwefwe',
+    #         '聯絡進度': '待聯絡',  # CSV 檔案中未提供這個資訊
+    #         '意願程度': 'None',  # CSV 檔案中未提供這個資訊
+    #         '聯絡方式': 'None'   # CSV 檔案中未提供這個資訊
+    #     }
+    # print(add_info_to_notion_database(NOTION_TOKEN, "db21b6d17cec4ed7be929bc3377e9ed9", data))
     # 使用函式
     # database_title = '霧峰區'
 
     # response = create_notion_database(NOTION_TOKEN,PAGE_ID,database_title)
     # print(response)
+    # districts = ["石岡區"]
+    # for district in districts:
+    #     database_id = create_notion_database(NOTION_TOKEN, PAGE_ID, f"臺中市{district}")
+    #     print(f"Database created for {district} District with ID: {database_id}")
+    
+    # CSV 檔案路徑與區域的對應
+    file_paths = {
+        '中區': 'community_crawlers/taichung/community/中區.csv',
+        '北區': 'community_crawlers/taichung/community/北區.csv',
+        '北屯區':'community_crawlers/taichung/community/北屯區.csv',
+        '南區': 'community_crawlers/taichung/community/南區.csv',
+        '大里區': 'community_crawlers/taichung/community/大里區.csv',
+        '太平區': 'community_crawlers/taichung/community/太平區.csv',
+        '大雅區': 'community_crawlers/taichung/community/大雅區.csv',
+        '大甲區': 'community_crawlers/taichung/community/大甲區.csv',
+        '大肚區': 'community_crawlers/taichung/community/大肚區.csv',
+        '和平區': 'community_crawlers/taichung/community/和平區.csv',
+        '東區': 'community_crawlers/taichung/community/東區.csv',
+        '東勢區': 'community_crawlers/taichung/community/東勢區.csv',
+        '梧棲區': 'community_crawlers/taichung/community/梧棲區.csv',
+        '清水區': 'community_crawlers/taichung/community/清水區.csv',
+        '潭子區': 'community_crawlers/taichung/community/潭子區.csv',
+        '烏日區': 'community_crawlers/taichung/community/烏日區.csv',
+        '石岡區': 'community_crawlers/taichung/community/石岡區.csv',
+        '神岡區': 'community_crawlers/taichung/community/神岡區.csv',
+        '西區': 'community_crawlers/taichung/community/西區.csv',
+        '西屯區': 'community_crawlers/taichung/community/西屯區.csv',
+        '南屯區': 'community_crawlers/taichung/community/南屯區.csv',
+        '豐原區': 'community_crawlers/taichung/community/豐原區.csv',
+        '沙鹿區': 'community_crawlers/taichung/community/沙鹿區.csv',
+        '后里區': 'community_crawlers/taichung/community/后里區.csv',
+        '新社區': 'community_crawlers/taichung/community/新社區.csv',
+        '外埔區': 'community_crawlers/taichung/community/外埔區.csv',
+        '大安區': 'community_crawlers/taichung/community/大安區.csv',
+        '龍井區': 'community_crawlers/taichung/community/龍井區.csv',
+        '霧峰區': 'community_crawlers/taichung/community/霧峰區.csv'
+    }
 
+    # 將各個區域名稱與對應的 ID 對應起來
+    district_id_map = {district['name']: district['id'] for district in districts_with_ids}
+    # 讀取 CSV 檔案並轉換為 data_example 格式，然後添加到 data_list
+    data_list = []
+    for district, file_path in file_paths.items():
+        district_data = pd.read_csv(file_path)
+        for _, row in district_data.iterrows():
+            population_str = str(row['population'])
+            # 检查字符串是否为 'nan'
+            if population_str.lower() == 'nan':
+                cleaned_population = 0
+            else:
+                cleaned_population = int(population_str.replace(',', '').replace(' 人', ''))
+            data_example = {
+                '對接窗口': row['contact_person'],
+                '電話': row['phone'],
+                'Email': row['email'] if pd.notna(row['email']) else 'None',
+                '社區名稱': row['name'],
+                '職稱': row['title'],
+                '人口數量':cleaned_population,
+                '地址': row['address'],
+                '聯絡進度': '待聯絡',  # CSV 檔案中未提供這個資訊
+                '意願程度': 'None',  # CSV 檔案中未提供這個資訊
+                '聯絡方式': 'None'   # CSV 檔案中未提供這個資訊
+            }
+            data_list.append({'database_id': district_id_map[district], 'data': data_example})
+    for item in data_list:
+        database_id = item['database_id']
+        data = item['data']
+        print(add_info_to_notion_database(NOTION_TOKEN, database_id, data))
+        time.sleep(0.1)
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
